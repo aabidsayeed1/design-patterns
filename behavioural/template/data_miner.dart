950ab55b73d6e110e904f186cae8970563b08846
Hadiya <hadiya@Hadiyas-MBP.local>
2023-10-10 18:07:10 +0500

    data miner abstract class

---
+++ behavioural/template/data_miner.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+abstract class DataMiner {
+  void mine(String path) {
+    File file = openFile(path);
+    String rawData = extractData(file);
+    String data = parseData(rawData);
+    String analysis = analyzeData(data);
+    sendReport(analysis);
+    closeFile(file);
+  }
+
+  File openFile(String path);
+  String extractData(File file);
+  String parseData(String rawData);
+  void closeFile(File file);
+
+  String analyzeData(String data) {
+    print('Analyzing data...');
+    return 'Amazing Data Analysis on $data';
+  }
+
+  void sendReport(String analysis) {
+    print('Sending data mining report: $analysis');
+  }
+}
